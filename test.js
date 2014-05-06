b60ecdf8166331f108f19aee03bd48c9ad182cd8
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-05-06 19:43:23 +0200

    cleanup

--- test.js
+++
@@ -1,7 +0,0 @@
-var gpio = require("pi-gpio");
-
-gpio.open(7, "output", function(err) {     // Open pin 16 for output
-    gpio.write(7, 1, function() {          // Set pin 16 high (1)
-        gpio.close(7);                     // Close pin 16
-    });
-});
